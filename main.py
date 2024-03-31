1fd0034ac37d670148645546fd4bb833d4b89065
vialaky <vialaky@gmail.com>
2024-03-31 21:59:52 +0300

    Set the language

--- main.py
+++ main.py
@@ -12,4 +12,21 @@ def encryption_or_decryption():
         return 2
 
 
+def english_or_russian():
+    print('Select the language:\n'
+          'e - English\n'
+          'r - Russian')
+    language = input()
+    while True:
+        if language in ['e', 'E', 'у', 'У']:
+            return 'e'
+        elif language in ['r', 'R', 'к', 'К']:
+            return 'r'
+        else:
+            print('Enter correct value:')
+            language = input()
+
+
+# Setting up the program
 print(encryption_or_decryption())
+print(english_or_russian())
