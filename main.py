ef3802c6ad98163f6992a2a304a474638f41b064
vialaky <vialaky@gmail.com>
2024-03-31 21:52:01 +0300

    Set the direction

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+def encryption_or_decryption():
+    print('Select the direction:\n'
+          '1 - encryption\n'
+          '2 - decryption')
+    direction = input()
+    while direction not in ['1', '2']:
+        print('Enter correct number:')
+        direction = input()
+    if direction == '1':
+        return 1
+    else:
+        return 2
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(encryption_or_decryption())
